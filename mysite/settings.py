93425db3207c62239974f445ef7a85401758cbb5
aaqidmasoodi <aaqidmasoodi@icloud.com>
2023-11-19 00:49:29 +0530

    added vercel configs

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,13 +12,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-nou&62th!*vrqv7+&dpra8p-v4j*39kf^48%&0py5!#i&!fh1t"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*", ".vercel.app"]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
